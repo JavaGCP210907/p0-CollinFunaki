d9d0ecc530cddafc3e8843bb5b884e4b4d2b9d1c
Collin Funaki <collinf@vt.edu>
2021-09-20 17:48:42 -0400

    Established successful connection to database

--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,10 +1,34 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.models.Menu;
+import com.revature.utils.ConnectionUtil;
+
 public class Launcher {
 	
 	public static void main(String[] args) {
+		//testing whether our connection is successful
+		try(Connection conn = ConnectionUtil.getConnection()){
+			System.out.println("Hello, connection was successful!");
+		} catch (SQLException e) {
+			System.out.println("Hey connection failed!");
+			e.printStackTrace();
+		}
 		
+		//here is the actual functionality of our application
+		
+		//create our menu object
+		//Menu menu = new Menu();
+		
+		//use the Menu's displayMenu() method to use the menu
+		//menu.displayMenu();
+		
+		//really clean main method right? Power of abstraction
 		
 		
 	}
+		
+		
 }
